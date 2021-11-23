687e3e30efbdbddd317cde888d02a78758b2e8c3
Sasha <Sasha@ACER>
2021-11-23 12:12:23 +0300

    Добавьте файлы проекта.

---
+++ task12/task12.cpp
@@ -0,0 +1,58 @@
+﻿// task12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+bool perevirka(char s[256]) {
+	int l = strlen(s);
+	bool b = true;
+	int i = 0;
+	while (i < l && b == true) {
+		if (s[i] < '0' || s[i]>'9') {
+			b = false;
+		}
+		i = i + 1;
+	}
+	return b;
+}
+int main()
+{
+	ifstream text;
+	ofstream text2;
+
+	char line[256];
+
+	text.open("text.txt");
+	text2.open("text2.txt", ios::binary);
+	while (!text.eof()) {
+		text.getline(line, 256);
+		char word[256] = {};          //Буфер для считывания строки
+		stringstream x;        //Создание потоковой переменной
+		x << line;
+
+
+		while (x >> word) {
+			if (perevirka(word) == true) {
+
+				text2.write((char*)&word, sizeof(int));
+
+
+				/*text2 << " ";*/
+			}
+		}
+		/*text2 << endl;*/
+	}
+	text2.close();
+	ifstream text22;
+	char line22[256];
+	text22.open("text2.txt", ios::binary);
+	while (text22.read((char*)&line22, sizeof(int))) {
+
+		cout << line22 << " ";
+	}
+	text.close();
+	text22.close();
+}
+
---
+++ task5/task5.cpp
@@ -0,0 +1,69 @@
+﻿// task5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+bool polindrom(char s[256], int k) {
+    int l = strlen(s);
+
+
+    k = (k - 1) / 2;
+    bool b = false;
+    int i = k;
+    while (i < l - k && b == false) {
+        int j = 1;
+        b = true;
+        while (j <= k && b == true) {
+            if (s[i - j] != s[i + j]) {
+                b = false;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    return b;
+}
+int main()
+{
+    int k;
+    cout << "vvedit k: ";
+    cin >> k;
+    ifstream text;
+
+    ofstream text2;
+    text.open("text.txt");
+    text2.open("text2.txt");
+
+    char line[256];
+    char line2[256];
+
+
+    while (!text.eof()) {
+        text.getline(line, 256);
+        /* cout << line << endl;*/
+        char word[256] = {};          //Буфер для считывания строки
+        stringstream x;        //Создание потоковой переменной
+        x << line;
+
+
+        while (x >> word) {
+            if (polindrom(word, k) == false) {
+                text2 << word;
+                text2 << " ";
+            }
+        }
+        text2 << endl;
+    }
+    text2.close();
+    ifstream text22;
+    text22.open("text2.txt");
+    while (!text22.eof()) {
+        text22.getline(line2, 256);
+        cout << line2 << endl;
+    }
+    text.close();
+    text22.close();
+}
+
